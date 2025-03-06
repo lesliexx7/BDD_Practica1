b7c3a63a3162e19dcf01729251ea5f77a9e6746a
ArthurDanMac <macias.galvan.arturodaniel@gmail.com>
2025-03-06 02:33:20 -0600

    Conultas agregadas

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -1,5 +1,24 @@
 SELECT * FROM dbo.cat_entidades;
 Select * from dbo.datoscovid;
+
+/************************************
+	Consulta No.1 Listar el top 5 de las entidades con más casos confirmados por cada uno de los años 
+	registrados en la base de datos. 
+	Significado de los valores de los catalogos: 
+	Responsable: Macias Galvan Arturo Daniel
+	Comentarios: 
+	
+*************************************/
+
+select top 5 cont as casos_confirmados,ENTIDAD_UM 
+from(
+	select ENTIDAD_UM,count(CLASIFICACION_FINAL)cont 
+	from datoscovid 
+	where CLASIFICACION_FINAL=3 
+	group by ENTIDAD_UM
+)as t1 
+order by casos_confirmados desc
+
 /************************************
 	Consulta No.3 Listar el porcentaje de casos confirmados en cada una 
 	de las siguientes morbilidades a nivel nacional: diabetes, obesidad e hipertensión. 
@@ -26,6 +45,21 @@ SELECT
 FROM datoscovid
 WHERE CLASIFICACION_FINAL IN (1, 2, 3);
 
+
+/************************************
+	Consulta No.4 Listar los municipios que no tengan casos confirmados en todas las morbilidades: 
+hipertensión, obesidad, diabetes, tabaquismo. 
+	Significado de los valores de los catalogos: 
+	Responsable: Macias Galvan Arturo Daniel
+	Comentarios: 
+	
+*************************************/
+select distinct(MUNICIPIO_RES)
+from datoscovid
+where not((HIPERTENSION=1 or OBESIDAD=1 or DIABETES=1 or TABAQUISMO=1) or 
+		CLASIFICACION_FINAL=1 or CLASIFICACION_FINAL=2 or CLASIFICACION_FINAL=3)
+
+
 /************************************
 	Consulta No.6 Listar el total de casos confirmados/sospechosos 
 	por estado en cada uno de los años registrados en la base de datos. 
@@ -44,7 +78,25 @@ GROUP BY ENTIDAD_RES, YEAR(TRY_CAST(FECHA_INGRESO AS DATE))
 ORDER BY Año, Total_Casos DESC;
 
 /************************************
-	Consulta No. 99.	Listar el top 3 de municipios con menos casos 
+	Consulta No.7 Para el año 2020 y 2021 cuál fue el mes con más casos registrados, confirmados, 
+sospechosos, por estado registrado en la base de datos. 
+	Significado de los valores de los catalogos: 
+	Responsable: Macias Galvan Arturo Daniel
+	Comentarios: 
+	
+*************************************/
+select distinct max(entidad) ent,FECHA_SINTOMAS 
+from(
+	select count(distinct ENTIDAD_UM)entidad,FECHA_SINTOMAS 
+	from datoscovid 
+	where (CLASIFICACION_FINAL=3 or CLASIFICACION_FINAL=6) and
+			(FECHA_SINTOMAS like '2020-%' or FECHA_SINTOMAS like'2021-%')
+	group by FECHA_SINTOMAS
+)as t1
+group by FECHA_SINTOMAS
+
+/************************************
+	Consulta No. 9.	Listar el top 3 de municipios con menos casos 
 	recuperados en el año 2021. 
 	Significado de los valores de los catalogos: 
 	Responsable: Palacios Reyes Leslie Noemi
@@ -63,6 +115,22 @@ WITH casos_recuperados AS (
 SELECT  TOP 3 * 
 FROM casos_recuperados 
 ORDER BY total_recuperados ASC, ENTIDAD_RES, MUNICIPIO_RES;
+
+/************************************
+	Consulta No.10  Listar el porcentaje de casos confirmado por género en los años 2020 y 2021.  
+	Significado de los valores de los catalogos: 
+	Responsable: Macias Galvan Arturo Daniel
+	Comentarios: 
+	
+*************************************/
+select SEXO,(
+		COUNT(CLASIFICACION_FINAL)*100.0 / (select count (CLASIFICACION_FINAL)
+												from datoscovid
+												where CLASIFICACION_FINAL=3 and 
+												(FECHA_SINTOMAS like '2020-%' or FECHA_SINTOMAS like'2021-%'))
+)casos from datoscovid 
+where CLASIFICACION_FINAL=3 and (FECHA_SINTOMAS like '2020-%' or FECHA_SINTOMAS like'2021-%') group by SEXO
+
 /************************************
 	Consulta No. 12
 	Significado de los valores de los catalogos: 
@@ -81,6 +149,46 @@ GROUP BY ce.entidad, YEAR(TRY_CAST(dc.FECHA_INGRESO AS DATE))
 ORDER BY ce.entidad, Año;
 
 
+/************************************
+	Consulta No.13  Listar porcentajes de casos confirmados por género en el rango de edades de 20 a 30 años, 
+	de 31 a 40 años, de 41 a 50 años, de 51 a 60 años y mayores a 60 años a nivel nacional.
+	Significado de los valores de los catalogos: 
+	Responsable: Macias Galvan Arturo Daniel
+	Comentarios: 
+	
+*************************************/
 
 
-
+select  distinct(select 
+(
+		COUNT(CLASIFICACION_FINAL)*100.0 / (select count (CLASIFICACION_FINAL)
+												from datoscovid
+												where CLASIFICACION_FINAL=3
+												)
+)from datoscovid where EDAD<=30 and EDAD>=20)anio20_30,
+		(select (
+		COUNT(CLASIFICACION_FINAL)*100.0 / (select count (CLASIFICACION_FINAL)
+												from datoscovid
+												where CLASIFICACION_FINAL=3
+												)
+) from datoscovid where EDAD>=31 and EDAD<=40)anio31_40,
+		(select (
+		COUNT(CLASIFICACION_FINAL)*100.0 / (select count (CLASIFICACION_FINAL)
+												from datoscovid
+												where CLASIFICACION_FINAL=3
+												)
+) from datoscovid where EDAD>=41 and EDAD<=50)anio41_50,
+		(select (
+		COUNT(CLASIFICACION_FINAL)*100.0 / (select count (CLASIFICACION_FINAL)
+												from datoscovid
+												where CLASIFICACION_FINAL=3
+												)
+) from datoscovid where EDAD>=51 and EDAD<=60)anio51_60,
+		(select (
+		COUNT(CLASIFICACION_FINAL)*100.0 / (select count (CLASIFICACION_FINAL)
+												from datoscovid
+												where CLASIFICACION_FINAL=3
+												)
+) from datoscovid where EDAD>60)anio60_mas
+		
+from datoscovid where CLASIFICACION_FINAL=3
