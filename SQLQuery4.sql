324a61338178c0a18c466b17208111f2a46ec016
MariFerLegorreta <mlegorretar2100@alumno.ipn.mx>
2025-03-05 23:12:45 -0600

    Update SQLQuery4.sql

--- SQLQuery4.sql
+++ SQLQuery4.sql
@@ -2,12 +2,16 @@ SELECT * FROM dbo.cat_entidades;
 Select * from dbo.datoscovid;
 /************************************
 	Consulta No.3 Listar el porcentaje de casos confirmados en cada una 
-	de las siguientes morbilidades a nivel nacional: diabetes, obesidad e hipertensión. 
+	de las siguientes morbilidades a nivel nacional: diabetes, obesidad e hipertensiÃ³n. 
 	Significado de los valores de los catalogos: 
 	Responsable: Palacios Reyes Leslie Noemi
 	Comentarios: 
 	
 *************************************/
+
+hiii
+
+
 SELECT 
     'Diabetes' AS Morbilidad,
     100.0 * SUM(CASE WHEN DIABETES = 1 THEN 1 ELSE 0 END) / COUNT(*) AS Porcentaje
@@ -21,31 +25,31 @@ FROM datoscovid
 WHERE CLASIFICACION_FINAL IN (1, 2, 3)
 UNION ALL
 SELECT 
-    'Hipertensión' AS Morbilidad,
+    'HipertensiÃ³n' AS Morbilidad,
     100.0 * SUM(CASE WHEN HIPERTENSION = 1 THEN 1 ELSE 0 END) / COUNT(*) AS Porcentaje
 FROM datoscovid
 WHERE CLASIFICACION_FINAL IN (1, 2, 3);
 
 /************************************
 	Consulta No.6 Listar el total de casos confirmados/sospechosos 
-	por estado en cada uno de los años registrados en la base de datos. 
+	por estado en cada uno de los aÃ±os registrados en la base de datos. 
 	Significado de los valores de los catalogos: 
 	Responsable: Palacios Reyes Leslie Noemi
 	Comentarios: 
 *************************************/
 SELECT 
     ENTIDAD_RES AS Estado,  
-    YEAR(TRY_CAST(FECHA_INGRESO AS DATE)) AS Año,  
+    YEAR(TRY_CAST(FECHA_INGRESO AS DATE)) AS AÃ±o,  
     COUNT(*) AS Total_Casos  
 FROM datoscovid  
 WHERE CLASIFICACION_FINAL IN (1, 2, 3)  -- 1, 2, 3 son casos confirmados o sospechosos  
 AND TRY_CAST(FECHA_INGRESO AS DATE) IS NOT NULL  
 GROUP BY ENTIDAD_RES, YEAR(TRY_CAST(FECHA_INGRESO AS DATE))  
-ORDER BY Año, Total_Casos DESC;
+ORDER BY AÃ±o, Total_Casos DESC;
 
 /************************************
 	Consulta No. 99.	Listar el top 3 de municipios con menos casos 
-	recuperados en el año 2021. 
+	recuperados en el aÃ±o 2021. 
 	Significado de los valores de los catalogos: 
 	Responsable: Palacios Reyes Leslie Noemi
 	Comentarios: 
@@ -71,14 +75,14 @@ ORDER BY total_recuperados ASC, ENTIDAD_RES, MUNICIPIO_RES;
 *************************************/
 SELECT 
     ce.entidad AS Estado,
-    YEAR(TRY_CAST(dc.FECHA_INGRESO AS DATE)) AS Año,
+    YEAR(TRY_CAST(dc.FECHA_INGRESO AS DATE)) AS AÃ±o,
     COUNT(*) AS Total_Casos_Negativos  
 FROM dbo.datoscovid dc
 JOIN dbo.cat_entidades ce ON dc.ENTIDAD_RES = ce.clave
 WHERE dc.CLASIFICACION_FINAL = 7  -- Casos negativos
 AND YEAR(TRY_CAST(dc.FECHA_INGRESO AS DATE)) IN (2020, 2021)
 GROUP BY ce.entidad, YEAR(TRY_CAST(dc.FECHA_INGRESO AS DATE))
-ORDER BY ce.entidad, Año;
+ORDER BY ce.entidad, AÃ±o;
 
 
 
